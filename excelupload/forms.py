02bc838284a52ecf25eae8301730319fb02e702e
wlgn8648 <wlgn8648@gmail.com>
2022-11-20 20:32:00 +0900

    excel_upload push

--- excelupload/forms.py
+++ excelupload/forms.py
@@ -7,3 +7,6 @@ class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = ('description', 'document',)
+        widgets = {
+            'document': forms.FileInput(attrs = {'class': 'form-control'})
+        }
--- excelupload/models.py
+++ excelupload/models.py
@@ -1,8 +1,9 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 
 # Create your models here.
 class Document(models.Model):
     description = models.CharField(max_length = 255, blank = True)
-    document = models.FileField(upload_to = 'documents/')
+    document = models.FileField(upload_to = 'documents/', validators = [FileExtensionValidator(allowed_extensions = ['xlsx'])])
     uploaded_at = models.DateTimeField(auto_now_add = True)
