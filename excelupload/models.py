e9a56763dd30b8b3d0ea0fd66db21f8e995c0c55
wlgn8648 <wlgn8648@gmail.com>
2022-11-23 20:11:32 +0900

    체크박스 추가

--- excelupload/models.py
+++ excelupload/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
 # Create your models here.
 class Document(models.Model):
-    student_number = models.CharField(max_length = 10, validators = [RegexValidator(regex = r'20\d{2}-\d{5}/', inverse_match = True)])
+    student_number = models.CharField(max_length = 10, validators = [RegexValidator(regex = r'20\d{2}-\d{5}/', inverse_match = True)], default = "")
     document = models.FileField(upload_to = 'documents/', validators = [FileExtensionValidator(allowed_extensions = ['xlsx'])])
     uploaded_at = models.DateTimeField(auto_now_add = True)
     degree_choices = (('석사', '석사'), ('박사', '박사'), ('통합', '통합'))
     degree = models.CharField(max_length = 10, choices = degree_choices, default = '석사')
     bootMath = models.BooleanField(default = False)
     bootCom = models.BooleanField(default = False)
+    paperTest = models.BooleanField(default = False)
