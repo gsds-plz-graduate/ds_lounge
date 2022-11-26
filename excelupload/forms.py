78238ac2144dfac3b3ec56bca81ac55499c0063e
wlgn8648 <wlgn8648@gmail.com>
2022-11-27 00:01:44 +0900

    모델 변경사항 저장

--- excelupload/forms.py
+++ excelupload/forms.py
@@ -8,19 +8,19 @@ class DocumentForm(forms.ModelForm):
 
     class Meta:
         model = Document
-        fields = ('student_number', 'degree', 'bootMath', 'bootCom', 'paperTest', 'document')
+        fields = ('student_number', 'degree', 'boot_math', 'boot_com', 'paper_test', 'document')
         labels = {
             'student_number': 'Student Number',
             'degree'        : 'Course',
-            'bootMath'      : 'Passed Mathematics/Statistics',
-            'bootCom'       : 'Passed Computing',
-            'paperTest'     : 'Passed Qualification Exam',
+            'boot_math'     : 'Passed Mathematics/Statistics',
+            'boot_com'      : 'Passed Computing',
+            'paper_test'    : 'Passed Qualification Exam',
             'document'      : 'Upload Excel',
         }
         widgets = {
-            'bootMath' : forms.CheckboxInput(),
-            'bootCom'  : forms.CheckboxInput(),
-            'paperTest': forms.CheckboxInput()
+            'boot_math' : forms.CheckboxInput(),
+            'boot_com'  : forms.CheckboxInput(),
+            'paper_test': forms.CheckboxInput()
         }
         help_texts = {
             'student_number': 'ex. 2022-12345',
