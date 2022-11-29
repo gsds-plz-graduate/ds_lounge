d1e12a28c45f7030d9c7bf660d99fe62b51d22b0
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2022-11-29 22:35:08 +0900

    Update settings.py
    Signed-off-by: Jihoo Park <67787453+park-jihoo@users.noreply.github.com>

--- group_project/settings.py
+++ group_project/settings.py
@@ -135,9 +135,11 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
 
 LOGIN_REDIRECT_URL = '/'
 
@@ -171,3 +173,20 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "https://*.bkms-team5-6nwxvl4rka-du.a.run.app"
 ]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
