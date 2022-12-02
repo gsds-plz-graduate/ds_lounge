ee4c4962b5065da57fc1f2d7a427db7660747e78
Hayley Son <hayleyson92@gmail.com>
2022-12-02 17:48:36 +0900

    Added updated recommendation logic. Updated requirements.txt.

--- recommendation/models.py
+++ recommendation/models.py
@@ -1,22 +1,47 @@
-# Create your models here.
+from allauth import app_settings
+from django.contrib.auth.middleware import get_user
 from django.core.validators import FileExtensionValidator, RegexValidator
 from django.db import models
+from check.models import CourseKey
 
 
-# Create your models here.
-class Document(models.Model):
-    student_number = models.CharField(
-        max_length=10,
-        validators=[RegexValidator(regex=r"20\d{2}-\d{5}/", inverse_match=True)],
-        default="",
+class Courses(models.Model):
+
+    # 혜령: models 에 대해 잘모르는데,
+    # 다른 파일 참고해서 rec_courses 테이블에 대응하는 class 정의해봤음.
+    # 필요 없으면 지워도 됨.
+    # 마음대로 수정해도되!
+    cid = models.ForeignKey(
+        primary_key=True, to=CourseKey, on_delete=models.CASCADE, db_column="cid"
     )
-    document = models.FileField(
-        upload_to="documents/",
-        validators=[FileExtensionValidator(allowed_extensions=["xlsx"])],
+    program = models.CharField(max_length=5, blank=True, null=True)
+    cname = models.CharField(max_length=100, blank=True, null=True)
+    avg_quota = models.FloatField(blank=True, null=True)
+    classification = models.CharField(max_length=5, blank=True, null=True)
+    dept = models.CharField(max_length=255, blank=True, null=True)
+    lang = models.CharField(max_length=50, blank=True, null=True)
+    yr_sem = models.CharField(max_length=255, blank=True, null=True)
+    last_yr_sem = models.CharField(max_length=10)
+    notes = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "rec_courses"
+
+
+class RecommendedResult(models.Model):
+
+    ### recommendation module 이 리턴하는 결과
+    ### 마음대로 수정해도되!
+    user = models.ForeignKey(
+        to=app_settings.USER_MODEL, verbose_name="user", on_delete=models.CASCADE
     )
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    degree_choices = (("석사", "Master"), ("박사", "Combined"), ("통합", "Ph.D"))
-    degree = models.CharField(max_length=10, choices=degree_choices, default="석사")
-    bootMath = models.BooleanField(default=False)
-    bootCom = models.BooleanField(default=False)
-    paperTest = models.BooleanField(default=False)
+    cid = models.ForeignKey(to=CourseKey, on_delete=models.CASCADE, db_column="cid")
+    cname = models.CharField(max_length=255, blank=True, null=True)
+    program = models.CharField(max_length=5, blank=True, null=True)
+    classification = models.CharField(max_length=5, blank=True, null=True)
+    crd = models.IntegerField(blank=True, null=True)
+    dept = models.CharField(max_length=255, blank=True, null=True)
+    lang = models.CharField(max_length=50, blank=True, null=True)
+    yr_sem = models.CharField(max_length=255, blank=True, null=True)
+    last_yr_sem = models.CharField(max_length=10, blank=True, null=True)
