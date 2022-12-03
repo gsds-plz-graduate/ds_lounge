252473fa94eda30e0a1f0f146ce9d22e782ba777
jihoopark <jihoopark@snu.ac.kr>
2022-12-03 16:06:02 +0900

    recommendation 추가

--- recommendation/views.py
+++ recommendation/views.py
@@ -1,37 +1,45 @@
 # Create your views here.
-from rest_framework.response import Response
-from .recommender import rec
-import pandas as pd
-
 import warnings
 
-warnings.filterwarnings(action="ignore")
+import pandas as pd
+from django.db.models import Max
+from django.shortcuts import render
+
+from check.models import Enrollment
+from .recommender import Recommender
+
+warnings.filterwarnings(action = "ignore")
+
+
 ## 다음 에러 때문에 filterwarnings 추가
 ## UserWarning: pandas only support SQLAlchemy connectable(engine/connection) ordatabase string URI or sqlite3 DBAPI2 connectionother DBAPI2 objects are not tested, please consider using SQLAlchemy
 
+def recommend(request):
+    user_id = request.user.id
+    recommended_courses = recommendation(uid = user_id)
+    return render(request, 'recommendation/recommendation.html', {'result': recommended_courses})
 
-def recommendation(uid, include_undergrad=True):
-    import psycopg2
 
-    connection_info = (
-        "host=147.47.200.145 dbname=teamdb5 user=team5 password=snugraduate port=34543"
-    )
-    conn = psycopg2.connect(connection_info)
+def recommendation(uid, include_undergrad = True):
 
-    query = (
-        "SELECT cid_int, user_id, cid from rec_enrollment where up_id = (select max(re.up_id) from rec_enrollment re where user_id = %s)"
-        % uid
-    )
+    # query = (
+    #     "SELECT cid_int, user_id, cid from rec_enrollment where up_id = (select max(re.up_id) from rec_enrollment re where user_id = %s)"
+    #     % uid
+    # )
 
+    recommend_courses = []
     try:
-        user_grade_df = pd.read_sql(query, conn)
-    except Exception as e:
-        print("Error: ", e)
-    finally:
-        conn.close()
+        user_up_id = Enrollment.objects.filter(user_id = uid).aggregate(Max('up_id'))['up_id__max']
+        user_grade_model = Enrollment.objects.filter(up_id = user_up_id)
+        user_grade_df = pd.DataFrame(list(user_grade_model.values('cid_int', 'user_id', 'cid')))
+        rec = Recommender(file_dir = "recommendation/data")
 
-    machine_learning_recommend_courses = rec.get_recommendation(
-        uid, user_grade_df, include_undergrad
-    )
-    recommend_courses = [machine_learning_recommend_courses.to_json()]
-    return Response(recommend_courses)
+        machine_learning_recommend_courses = rec.get_recommendation(
+            uid, user_grade_df, include_undergrad
+        )
+
+        recommend_courses = [machine_learning_recommend_courses.to_json()]
+    except Enrollment.DoesNotExist:
+        recommend_courses = []
+
+    return recommend_courses
