162f6bb642054d7d71f41ab84de6d5c546f17ae3
jihoopark <jihoopark@snu.ac.kr>
2022-12-04 14:13:14 +0900

    오류수정

--- common/urls.py
+++ common/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path
+
+from common import views
+
 app_name = "common"
 
 urlpatterns = [
-    
+    path('inclugrd/', views.inclugrd, name = "inclugrd"),
+    path('shreyn/', views.shreyn, name = "shreyn"),
+    path('share/', views.share, name = "share"),
 ]
